1709d02e0b1f9d97d81b64b0bc7b5facc13190cd
Randhier <randhier@gmail.com>
2018-11-27 20:12:29 -0500

    Simulation cleanup

--- src/test/scala/com/spring/resting/InitialSimulation.scala
+++ src/test/scala/com/spring/resting/InitialSimulation.scala
@@ -3,11 +3,11 @@ import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
 
-class Simulation extends Simulation {
+class InitialSimulation extends Simulation {
   private val baseUrl = "http://localhost:8080/"
   private val contentType = "application/json"
   private val endpoint = "greetings"
-  private val requestCount = 10
+  private val requestCount = 100
 
 
   val httpProtocol: HttpProtocolBuilder = http
@@ -17,13 +17,12 @@ class Simulation extends Simulation {
     .contentTypeHeader(contentType)
     .userAgentHeader("curl/7.54.0")
 
-  val headers_0 = Map("Expect" -> "100-continue")
 
-  val scn: ScenarioBuilder = scenario("RecordedSimulation")
+  val scn: ScenarioBuilder = scenario("Simulation")
     .exec(http("request_0")
       .get(endpoint)
-      .headers(headers_0)
-      .check(status.is(200)))
+      .check(status.is(200))
+    )
 
   setUp(scn.inject(atOnceUsers(requestCount))).protocols(httpProtocol)
 }
